e873fb99a7a62ab07218f0ce18d15b1f1d8407f5
James Clancey <james.clancey@gmail.com>
2017-10-13 14:20:38 -0800

    Added Key back in

---
+++ Sample/AppleMusicSample/ApiKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AppleMusicSample
+{
+	internal static class ApiKeys
+	{
+		internal static string AppleMusicKeyId {get;set;}
+		internal static string TeamId {get;set;}
+		internal static string PrivateSigningKey { get; set; }
+
+		internal static string DeveloperToken { get; set; }
+		internal static DateTime DeveloperTokenExpiration { get; set; }
+	}
+}
